b97307d864a9fd0d2482315f39989b21c3f2c0e8
Pascal Floride <pfloride@weluse.de>
2015-02-05 00:15:19 +0100

    Revert "Merge pull request #2 from PortChaw/revert-1-master"
    This reverts commit c985ea260e5dad6bcbf616e4231eae552747a973, reversing
    changes made to 91a28a09feaaf91ebbb5638f4ddd7f2b05661912.

--- source/js/ng-img-crop.js
+++ source/js/ng-img-crop.js
@@ -9,8 +9,9 @@ crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function($timeo
 
       changeOnFly: '=',
       areaType: '@',
+      aspectRatio: '=',
       areaMinSize: '=',
-      resultImageSize: '=',
+      resultImageSize: '@',
 
       onChange: '&',
       onLoadBegin: '&',
@@ -79,6 +80,10 @@ crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function($timeo
       });
       scope.$watch('areaType',function(){
         cropHost.setAreaType(scope.areaType);
+        updateResultImage(scope);
+      });
+      scope.$watch('aspectRatio',function(){
+        cropHost.setAspectRatio(scope.aspectRatio);
         updateResultImage(scope);
       });
       scope.$watch('areaMinSize',function(){
